fdba80251d18c6c637e530a6349c27ee00e012a1
Akudbilla Daniel <akuddev@gmail.com>
2022-02-08 15:26:36 +0000

    added js

---
+++ js/index.js
@@ -0,0 +1,16 @@
+window.onload = () => {
+  const progressBars = document.querySelectorAll("progress-bar");
+  const progressPercent = document.querySelectorAll("percentage_val");
+  const finishButton = document.querySelector("finish-btn");
+
+  finishButton.addEventListener("click", () => {
+    console.log("hello");
+    progressBars.forEach((bar) => {
+      bar.style.width = "100%";
+    });
+
+    progressPercent.forEach((bar) => {
+      bar.innerText = "100%";
+    });
+  });
+};
